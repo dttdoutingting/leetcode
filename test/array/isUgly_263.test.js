58161ef0b34adc80a72d1d8e87d11c4c91c6357c
Mingqiang Sun <mingqiang.sun@weimob.com>
2020-02-18 13:38:18 +0800

    丑数-263

---
+++ test/array/isUgly_263.test.js
@@ -0,0 +1,15 @@
+import testIsUgly from '../../code/array/isUgly_263'
+
+test('isUgly: 1', () => {
+    expect(testIsUgly(8)).toBe(true)
+})
+test('isUgly: 2', () => {
+    expect(testIsUgly(-8)).toBe(false)
+})
+test('isUgly: 3', () => {
+    expect(testIsUgly(937351770)).toBe(false)
+})
+
+test('isUgly: 4', () => {
+    expect(testIsUgly(6)).toBe(true)
+})
